7a1a3c494a925a75c15cf3a4e7ac0dc31cef52c1
tonysoprano02 <106454378+tonysoprano02@users.noreply.github.com>
2022-06-08 12:45:53 -0700

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,7 @@ import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['../home.page.scss'],
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
 
